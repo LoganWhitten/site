6621472ff10b3f5f4a0c4bb24ca8dedce6d36a80
LOGAN WHITTEN <lights9999@hotmail.com>
2025-06-28 17:12:04 -0400

    fix: lightmode cards/metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,15 @@
 import { ReactNode } from "react";
 import "./globals.css"
 import { ReactLenis } from "lenis/react";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Logan Whitten's Site",
+  description: "Logan Whitten is a Lighting Technician & Developer currently studying at UNCSA.",
+  metadataBase: new URL("https://next-learn-dashboard.vercel.sh"),
+};
+
+
 export default function RootLayout({children}: {children: ReactNode}){
   return (
     <html>
