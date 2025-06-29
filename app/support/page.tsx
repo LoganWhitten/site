279534f220bf6b6dec34c344a244eba48356d2ad
LOGAN WHITTEN <lights9999@hotmail.com>
2025-06-29 15:01:14 -0400

    add support page

---
+++ app/support/page.tsx
@@ -0,0 +1,7 @@
+export default function Page(){
+    return(
+        <div className="flex h-screen place-content-center place-items-center gap-1">
+            For sACN viewer support please email me at: <a href="mailto:logan@loganwhitten.com">Logan@LoganWhitten.com</a>
+        </div>
+    )
+}
