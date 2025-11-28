d628cd3d3e4607852bd198656f20cb61c5f9727f
LOGAN WHITTEN <lights9999@hotmail.com>
2025-11-28 17:06:44 -0600

    ux: clarify experimentation

--- components/banner.tsx
+++ components/banner.tsx
@@ -3,13 +3,14 @@
 export default function Banner() {
   return (
     <div className="fixed no-print top-0 w-screen z-50 bg-black text-white outline outline-white/50 rounded-b-sm p-1 text-center font-semibold">
-      Made very quickly by {" "}
+      An experiment by {" "}
       <a
         className="hover:underline"
         href="https://instagram.com/loganwhitten512"
       >
         Logan Whitten
       </a>
+      {" | "} Not recomended in production
     </div>
   );
 }
