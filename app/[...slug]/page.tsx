1bf3b2f6e6a01ee05c6ee742a7fdba229a6c110e
LOGAN WHITTEN <lights9999@hotmail.com>
2026-03-13 12:38:43 -0400

    fix: redirection

--- app/[...slug]/page.tsx
+++ app/[...slug]/page.tsx
@@ -1,6 +1,7 @@
 import { redirect } from "next/navigation";
 
-export default async function Page(props: PageProps<"/[...slug]">) {
-  const { slug } = await props.params;
-  redirect(`https://dev.loganwhitten.com/${slug}`)
+export default async function Page({ params }: { params: Promise<{ slug: string[] }> }) {
+  const { slug } = await params;
+  const path = Array.isArray(slug) ? slug.join('/') : slug;
+  redirect(`https://dev.loganwhitten.com/${path}`);
 }
---
+++ app/boom/page.tsx
@@ -0,0 +1,14 @@
+export default function Page() {
+  return (
+    <div className="flex h-full place-items-center place-content-center">
+      for support, contact me at{" "}
+      <a
+        className="pl-1 underline hover:no-underline"
+        href="mailto:logan@loganwhitten.com"
+      >
+        {" "}
+        logan@loganwhitten.com
+      </a>
+    </div>
+  );
+}
