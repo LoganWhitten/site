ed0bd5bb9eeae9b7012ccb557250f3c9f0cc6a1b
Logan Whitten <32558210+LoganWhitten@users.noreply.github.com>
2026-08-25 20:58:26 -0400

    ui: content

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function NewHome() {
           title="Design & Production | UNC School of the Arts"
           subtitle="Production Electrician, ETC EOS, Grandma3."
           number="'27"
-          gallery={["/images/integration.jpeg"]}
+          gallery={["/images/school.jpeg"]}
           img="/images/school.jpeg"
           activeTab={activeTab}
           setActive={setActive}
@@ -66,7 +66,7 @@ export default function NewHome() {
         <HeroItem
           name="Illuminated Integration"
           title="Systems Integrator Intern"
-          gallery={["/images/integration.jpeg"]}
+          gallery={["/images/coaster.jpg"]}
           subtitle="Q-Sys Plugin Development, Cisco Networking."
           number="'25"
           img="/images/coaster.jpg"
@@ -92,7 +92,7 @@ export default function NewHome() {
         <HeroItem
           name="Side Projects"
           title="Portfolio Website"
-          gallery={["/images/integration.jpeg"]}
+          gallery={["/images/boom.jpeg"]}
           subtitle="Website/App Development, Brand Management."
           number=""
           img="/images/boom.jpeg"
